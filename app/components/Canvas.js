a409329c8e361f53cc0c83f373c74a00f63bde2f
javinor <javinor@gmail.com>
2016-11-16 01:01:10 +0200

    basic user input functionality

---
+++ app/components/Canvas.js
@@ -0,0 +1,19 @@
+'use strict'
+
+import React from 'react'
+
+class Canvas extends React.Component {
+  render() {
+    return (
+      <div>
+        <canvas style={{border: '1px solid blue'}}>
+          Canvas not supported!?
+        </canvas>
+      </div>
+    )
+  }
+}
+
+
+
+export default Canvas
--- app/main.js
+++ app/main.js
@@ -3,21 +3,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Hello extends React.Component {
+import Canvas from './components/Canvas'
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {iterations: 0}
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+  }
+
+  increment(e) {
+    this.setState({iterations: this.state.iterations + 1})
+  }
+
+  decrement() {
+    this.setState({iterations: this.state.iterations - 1})
+  }
+
+  componentDidUpdate() {
+    console.log('in comp did update')
+    console.log(this.state.iterations)
+  }
+
   render() {
     return (
       <div>
         <div>
-          <button>-</button>
-          <span>4</span>
-          <button>+</button>
+          <button onClick={this.decrement}>-</button>
+          <span>{this.state.iterations}</span>
+          <button onClick={this.increment}>+</button>
         </div>
-        <div>
-          <canvas></canvas>
-        </div>
+        <Canvas />
       </div>
     )
   }
 }
 
-ReactDOM.render(<Hello />, document.getElementById('app'))
+ReactDOM.render(<Main />, document.getElementById('app'))
