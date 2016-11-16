43f3a2989dc2b45241334695debd2453920e3c0a
javinor <javinor@gmail.com>
2016-11-16 03:18:41 +0200

    removing logic from canvas

--- app/components/CanvasComponent.js
+++ app/components/CanvasComponent.js
@@ -2,62 +2,40 @@
 
 import React from 'react'
 
-import {RULES, ACTIONS} from '../constants'
-
 class CanvasComponent extends React.Component {
   constructor(props) {
     super(props)
-
-    this.getStartPosition = this.getStartPosition.bind(this)
-    this.updateCanvas = this.updateCanvas.bind(this)
+    this.canvasReadyToUpdate = this.canvasReadyToUpdate.bind(this)
   }
 
-  getStartPosition() {
-    return { 
-      x: this.props.width / 3,
-      y: this.props.height * 2 / 5,
-      angle: Math.PI / 2,
-      length: this.props.width / Math.pow(3, this.props.iterations)
-    }
-  }
-  componentDidMount() { this.updateCanvas() }
-  componentDidUpdate() { this.updateCanvas() }
+  componentDidMount() { window.requestAnimationFrame(this.canvasReadyToUpdate) }
+  componentDidUpdate() { window.requestAnimationFrame(this.canvasReadyToUpdate) }
 
-  updateCanvas() {
-    let pencil = this.getStartPosition()
-
+  canvasReadyToUpdate() {
     const ctx = this.refs.canvas.getContext('2d')
-    ctx.clearRect(0, 0, this.props.width, this.props.height)
-    ctx.beginPath()
-    ctx.strokeStyle = this.props.strokeStyle
-    ctx.moveTo(pencil.x, pencil.y)
-
-    for (let i = 0; i < this.props.path.length; i += 1) {
-      pencil = ACTIONS[this.props.path[i]](pencil)
-      ctx.lineTo(pencil.x, pencil.y)
-    }
-
-    ctx.stroke()
-    ctx.closePath()
+    this.props.onCanvasReadyToUpdate(ctx)
   }
 
   render() {
+    const style = {
+      // border: '2px solid blue'
+    }
+
     return (
-      <div>
-        <canvas 
-          ref='canvas'
-          height={this.props.height}
-          width={this.props.width}
-          style={{border: '1px solid blue'}}>
-          Canvas not supported!?
-        </canvas>
-      </div>
+      <canvas 
+        ref='canvas'
+        height={this.props.height}
+        width={this.props.width}
+        style={style}>
+        Canvas not supported!?
+      </canvas>
     )
   }
 }
 
 CanvasComponent.defaultProps = {
-  strokeStyle: '#000'
+  height: '300',
+  width: '400'
 }
 
 export default CanvasComponent
--- app/components/Koch.js
+++ app/components/Koch.js
@@ -3,10 +3,24 @@
 import React from 'react'
 
 import CanvasComponent from './CanvasComponent'
-import {RULES} from '../constants'
+import {RULES, ACTIONS} from '../constants'
 
 class Koch extends React.Component {
-  
+  constructor(props) {
+    super(props)
+    this.getStartPosition = this.getStartPosition.bind(this)
+    this.updateCanvas = this.updateCanvas.bind(this)
+  }
+
+  getStartPosition() {
+    return { 
+      x: this.props.width / 3,
+      y: this.props.height * 2 / 5,
+      angle: Math.PI / 2,
+      length: this.props.width / Math.pow(3, this.props.iterations)
+    }
+  }
+
   getNthPath(n) {
     const paths = [RULES.start]
 
@@ -19,16 +33,31 @@ class Koch extends React.Component {
     return paths[n - 1]
   }
 
+  updateCanvas(ctx) {
+    const path = this.getNthPath(this.props.iterations)
+    let pencil = this.getStartPosition()
+
+    ctx.clearRect(0, 0, this.props.width, this.props.height)
+    ctx.beginPath()
+    ctx.strokeStyle = this.props.strokeStyle
+    ctx.moveTo(pencil.x, pencil.y)
+
+    for (let i = 0, len = path.length; i < len; i += 1) {
+      pencil = ACTIONS[path[i]](pencil)
+      ctx.lineTo(pencil.x, pencil.y)
+    }
+
+    ctx.stroke()
+    ctx.closePath()
+  }
+
   render() {
     return (
-      <div>
-        <CanvasComponent 
-          height={this.props.height}
-          width={this.props.width}
-          path={this.getNthPath(this.props.iterations)}
-          iterations={this.props.iterations}
-        />
-      </div>
+      <CanvasComponent 
+        height={this.props.height}
+        width={this.props.width}
+        onCanvasReadyToUpdate={this.updateCanvas}
+      />
     )
   }
 }
@@ -36,6 +65,7 @@ class Koch extends React.Component {
 Koch.defaultProps = {
   height: '300',
   width: '400',
+  strokeStyle: '#000'
 }
 
 export default Koch
